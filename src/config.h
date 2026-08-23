ede72cf6ff8f248915a23ece799b0ef3c140cd6c
kRxZy <192328467+kRxZykRxZy@users.noreply.github.com>
2026-08-23 09:23:14 +0100

    Refactor monitor into modular source

---
+++ src/config.h
@@ -0,0 +1,10 @@
+#ifndef PI_MONITOR_CONFIG_H
+#define PI_MONITOR_CONFIG_H
+
+#define PI_MONITOR_PORT 80
+#define PI_MONITOR_USER "admin"
+#define PI_MONITOR_PASSWORD "Hm361485%"
+#define PI_MONITOR_WEB_ROOT "/usr/share/pi-monitor/web"
+#define PI_MONITOR_COOKIE "PISESSION=ok"
+
+#endif
