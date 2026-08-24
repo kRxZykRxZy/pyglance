ab478cddf7c7a72bf627917dea3c5aedcf0bdd86
kRxZy <192328467+kRxZykRxZy@users.noreply.github.com>
2026-08-24 17:16:58 +0100

    Add control-plane telemetry APIs

--- src/system.h
+++ src/system.h
@@ -7,5 +7,10 @@ void ports_json(char *out, size_t out_size);
 void logs_json(char *out, size_t out_size);
 void disks_json(char *out, size_t out_size);
 void files_json(char *out, size_t out_size, const char *path);
+void network_json(char *out, size_t out_size);
+void services_json(char *out, size_t out_size);
+void memory_json(char *out, size_t out_size);
+void cpu_json(char *out, size_t out_size);
+void system_info_json(char *out, size_t out_size);
 int process_signal(int pid, int sig);
 #endif
