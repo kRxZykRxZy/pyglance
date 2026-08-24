2ad40873b96edcbd7fc842422b1a738d9e535e16
kRxZy <192328467+kRxZykRxZy@users.noreply.github.com>
2026-08-24 17:05:41 +0100

    Add disk inventory and filesystem browser APIs

--- src/system.h
+++ src/system.h
@@ -1,12 +1,11 @@
 #ifndef PI_MONITOR_SYSTEM_H
 #define PI_MONITOR_SYSTEM_H
-
 #include <stddef.h>
-
 void system_status_json(char *out, size_t out_size);
 void processes_json(char *out, size_t out_size);
 void ports_json(char *out, size_t out_size);
 void logs_json(char *out, size_t out_size);
+void disks_json(char *out, size_t out_size);
+void files_json(char *out, size_t out_size, const char *path);
 int process_signal(int pid, int sig);
-
 #endif
